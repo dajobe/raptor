1c0ab3f0cfeb30ebc2f78aa0831b099afae4a730
Dave Beckett <dave@dajobe.org>
2001-01-25 19:14:37 +0000

    Fixed base URI handling, updated to new rapier_new api Added -r, --replace-newlines for replacing newlines with spaces in literals Added -q, --quiet for less messages.

--- utils/rapper.c
+++ utils/rapper.c
@@ -99,11 +99,27 @@ static void print_statements(void *user_data, const rapier_statement *statement)
 int main(int argc, char *argv[]);
 
 
+/* replace newlines in literal string output with spaces */
+static int replace_newlines=0;
+
+/* extra noise? */
+static int quiet=0;
+
 
 static
 void print_statements(void *user_data, const rapier_statement *statement) 
 {
   fputs("rdfdump: Statement: ", stdout);
+
+  /* replace newlines with spaces if object is a literal string */
+  if(replace_newlines && 
+     statement->object_type == RAPIER_OBJECT_TYPE_LITERAL) {
+    char *s;
+    for(s=(char*)statement->object; *s; s++)
+      if(*s == '\n')
+        *s=' ';
+  }
+
   rapier_print_statement(statement, stdout);
   fputc('\n', stdout);
 }
@@ -116,7 +132,7 @@ void print_statements(void *user_data, const rapier_statement *statement)
 #endif
 
 
-#define GETOPT_STRING "sh"
+#define GETOPT_STRING "shrq"
 
 #ifdef HAVE_GETOPT_LONG
 static struct option long_options[] =
@@ -124,6 +140,8 @@ static struct option long_options[] =
   /* name, has_arg, flag, val */
   {"scan", 0, 0, 's'},
   {"help", 0, 0, 'h'},
+  {"replace-newlines", 0, 0, 'r'},
+  {"quiet", 0, 0, 'q'},
   {NULL, 0, 0, 0}
 };
 #endif
@@ -134,15 +152,18 @@ int
 main(int argc, char *argv[]) 
 {
   rapier_parser* parser;
-  char *uri;
+  char *uri_string;
+  char *base_uri_string;
   char *program=argv[0];
   int rc;
   int scanning=0;
   int usage=0;
 #ifdef LIBRDF_INTERNAL
   librdf_uri *base_uri;
+  librdf_uri *uri;
 #else
   const char *base_uri;
+  const char *uri;
 #endif
 
 #ifdef LIBRDF_INTERNAL
@@ -181,36 +202,60 @@ main(int argc, char *argv[])
       case 's':
         scanning=1;
         break;
+
+      case 'q':
+        quiet=1;
+        break;
+
+      case 'r':
+        replace_newlines=1;
+        break;
     }
     
   }
-  
-  if(optind != argc-1)
+
+  if(optind != argc-1 && optind != argc-2)
     usage=2; /* usage and error */
   
 
   if(usage) {
-    fprintf(stderr, "Usage: %s [OPTIONS] <RDF source file: URI>\n", program);
+    fprintf(stderr, "Usage: %s [OPTIONS] <source file: URI> [base URI]\n", program);
     fprintf(stderr, "Parse the given file as RDF using Rapier\n");
     fprintf(stderr, HELP_TEXT(h, help, "This message"));
     fprintf(stderr, HELP_TEXT(s, scan, "Scan for <rdf:RDF> element in source"));
+    fprintf(stderr, HELP_TEXT(r, replace-newlines, "Replace newlines with spaces in literals"));
+    fprintf(stderr, HELP_TEXT(q, quiet, "No extra information messages"));
     return(usage>1);
   }
 
-  uri=argv[optind];
+
+  if(optind == argc-1)
+    uri_string=base_uri_string=argv[optind];
+  else {
+    uri_string=argv[optind++];
+    base_uri_string=argv[optind];
+  }
+  
 
 #ifdef LIBRDF_INTERNAL
-  base_uri=librdf_new_uri(uri);
+  base_uri=librdf_new_uri(base_uri_string);
   if(!base_uri) {
     fprintf(stderr, "%s: Failed to create librdf_uri for %s\n",
             program, uri);
     return(1);
   }
+  uri=librdf_new_uri(uri_string);
+  if(!uri) {
+    fprintf(stderr, "%s: Failed to create librdf_uri for %s\n",
+            program, uri);
+    return(1);
+  }
 #else
-  base_uri=uri;
+  uri=uri_string;
+  base_uri=base_uri_string;
 #endif
   
-  parser=rapier_new(base_uri);
+  parser=rapier_new();
   if(!parser) {
     fprintf(stderr, "%s: Failed to create rapier parser\n", program);
     return(1);
@@ -221,12 +266,19 @@ main(int argc, char *argv[])
     rapier_set_feature(parser, RAPIER_FEATURE_SCANNING, 1);
   
 
-  /* PARSE the URI as RDF/XML*/
-  fprintf(stdout, "%s: Parsing URI %s\n", program, uri);
-
+  /* PARSE the URI as RDF/XML */
+  if(!quiet) {
+    if(base_uri_string)
+      fprintf(stdout, "%s: Parsing URI %s with base URI %s\n", program,
+              uri_string, base_uri_string);
+    else
+      fprintf(stdout, "%s: Parsing URI %s\n", program, uri_string);
+  }
+  
   rapier_set_statement_handler(parser, NULL, print_statements);
 
-  if(rapier_parse_file(parser, uri, uri)) {
+
+  if(rapier_parse_file(parser, uri, base_uri)) {
     fprintf(stderr, "%s: Failed to parse RDF into model\n", program);
     rc=1;
   } else
