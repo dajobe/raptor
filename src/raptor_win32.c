cdb23e5c91142cc21146377caffa31656318d1bd
Dave Beckett <dave@dajobe.org>
2002-08-18 21:57:49 +0000

    Raptor WIN32 support functions

---
+++ src/raptor_win32.c
@@ -0,0 +1,46 @@
+/* -*- Mode: c; c-basic-offset: 2 -*-
+ *
+ * raptor_win32.c - Raptor WIN32 support functions
+ *
+ * $Id$
+ *
+ * Copyright (C) 2002 David Beckett - http://purl.org/net/dajobe/
+ * Institute for Learning and Research Technology - http://www.ilrt.org/
+ * University of Bristol - http://www.bristol.ac.uk/
+ * 
+ * This package is Free Software or Open Source available under the
+ * following licenses (these are alternatives):
+ *   1. GNU Lesser General Public License (LGPL)
+ *   2. GNU General Public License (GPL)
+ *   3. Mozilla Public License (MPL)
+ * 
+ * See LICENSE.html or LICENSE.txt at the top of this package for the
+ * full license terms.
+ * 
+ */
+
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef WIN32
+#include <win32_config.h>
+#endif
+
+
+#ifdef WIN32
+
+/* Only on WIN32 systems */
+
+
+/* DLL entry point */
+BOOL APIENTRY
+DllMain(HANDLE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+  return TRUE;
+}
+
+
+/* end if WIN32 */
+#endif
