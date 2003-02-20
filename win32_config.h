eb8bf6eda710599d21efdfa70bb1f77fb194650d
Dave Beckett <dave@dajobe.org>
2003-02-20 16:33:23 +0000

    win32 has C99 compatible vsnprintf called _vsnprintf

--- win32_config.h
+++ win32_config.h
@@ -29,13 +29,27 @@ extern "C" {
 #endif
 
 #define WIN32_LEAD_AND_MEAN 1
+
+/* Compiling inside Raptor */
+#define RAPTOR_INTERNAL 1
+
+/* getopt is not in standard win32 C library - define if we have it */
+/* #define HAVE_GETOPT_H 1 */
+
 #define HAVE_STDLIB_H 1
 
-/* use expat on win32 */
+/* For using expat on win32 */
 #define RAPTOR_XML_EXPAT 1
-#define HAVE_XMLPARSE_H 1
+#define HAVE_EXPAT_H 1
 
-#define HAVE_STRCMPI
+#define HAVE_STRICMP 1
+
+/* MS names for these functions */
+#define vsnprintf _vsnprintf
+#define snprintf _snprintf
+
+#define HAVE_C99_VSNPRINTF 1
+
 
 #include <windows.h>
 
