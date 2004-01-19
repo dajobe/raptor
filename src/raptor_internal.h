b6a7c4acc389aa38af1cd01204d767e2b81eb5af
Dave Beckett <dave@dajobe.org>
2004-01-19 00:01:25 +0000

    Define SYSTEM_MALLOC, SYSTEM_FREE

--- src/raptor_internal.h
+++ src/raptor_internal.h
@@ -57,6 +57,16 @@ extern "C" {
 #define RAPTOR_DEBUG5(msg, arg1, arg2, arg3, arg4) do {fprintf(stderr, "%s:%d:%s: " msg, __FILE__, __LINE__, __func__, arg1, arg2, arg3, arg4);} while(0)
 #define RAPTOR_DEBUG6(msg, arg1, arg2, arg3, arg4, arg5) do {fprintf(stderr, "%s:%d:%s: " msg, __FILE__, __LINE__, __func__, arg1, arg2, arg3, arg4, arg5);} while(0)
 
+#if defined(HAVE_DMALLOC_H) && defined(LIBRDF_MEMORY_DEBUG_DMALLOC)
+void* raptor_system_malloc(size_t size);
+void raptor_system_free(void *ptr);
+#define SYSTEM_MALLOC(size)   raptor_system_malloc(size)
+#define SYSTEM_FREE(ptr)   raptor_system_free(ptr)
+#else
+#define SYSTEM_MALLOC(size)   malloc(size)
+#define SYSTEM_FREE(ptr)   free(ptr)
+#endif
+
 #else
 /* DEBUGGING TURNED OFF */
 
@@ -67,6 +77,9 @@ extern "C" {
 #define RAPTOR_DEBUG4(msg, arg1, arg2, arg3)
 #define RAPTOR_DEBUG5(msg, arg1, arg2, arg3, arg4)
 #define RAPTOR_DEBUG6(msg, arg1, arg2, arg3, arg4, arg5)
+
+#define SYSTEM_MALLOC(size)   malloc(size)
+#define SYSTEM_FREE(ptr)   free(ptr)
 
 #endif
 
