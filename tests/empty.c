13aa64f4c62d9eac6e57f1af66e8009c744d26be
Dave Beckett <dave@dajobe.org>
2004-06-12 04:34:12 +0000

    Test of empty C file, just using raptor.h work

---
+++ tests/empty.c
@@ -0,0 +1,30 @@
+/* -*- Mode: c; c-basic-offset: 2 -*-
+ *
+ * empty.c - Raptor empty file test
+ *
+ * $Id$
+ *
+ * Copyright (C) 2004 David Beckett - http://purl.org/net/dajobe/
+ * Institute for Learning and Research Technology - http://www.ilrt.bris.ac.uk/
+ * University of Bristol - http://www.bristol.ac.uk/
+ * 
+ * This package is Free Software or Open Source available under the
+ * following licenses (these are alternatives):
+ *   1. GNU Lesser General Public License (LGPL)
+ *   2. GNU General Public License (GPL)
+ *   3. Mozilla Public License (MPL)
+ * 
+ * See LICENSE.html or LICENSE.txt at the top of this package for the
+ * full license terms.
+ * 
+ */
+
+#include <raptor.h>
+
+int main(int argc, char *argv[]);
+
+int
+main(int argc, char *argv[])
+{
+  return 0;
+}
