5d26f7063ec293589782c6629768e6e5ca303e43
Dave Beckett <dave@dajobe.org>
2004-10-19 23:08:41 +0000

    Updated raptor_new_iostream_to_string with malloc_handler argument

--- src/raptor.h
+++ src/raptor.h
@@ -476,7 +476,7 @@ RAPTOR_API raptor_iostream* raptor_new_iostream_from_handler(void *context, rapt
 RAPTOR_API raptor_iostream* raptor_new_iostream_to_sink(void);
 RAPTOR_API raptor_iostream* raptor_new_iostream_to_filename(const char *filename);
 RAPTOR_API raptor_iostream* raptor_new_iostream_to_file_handle(FILE *fh);
-RAPTOR_API raptor_iostream* raptor_new_iostream_to_string(void **string, size_t *length_p);
+RAPTOR_API raptor_iostream* raptor_new_iostream_to_string(void **string, size_t *length_p, void *(*malloc_handler)(size_t size));
 RAPTOR_API void raptor_free_iostream(raptor_iostream *iostr);
 RAPTOR_API int raptor_iostream_write_bytes(raptor_iostream *iostr, const void *ptr, size_t size, size_t nmemb);
 RAPTOR_API int raptor_iostream_write_byte(raptor_iostream *iostr, const int byte);
