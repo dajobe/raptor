8811e208e19be8f04825668e6684f5efe1c04bf4
Dave Beckett <dave@dajobe.org>
2005-01-15 19:26:45 +0000

    Bumped version to 1.4.5

--- src/win32_raptor_config.h
+++ src/win32_raptor_config.h
@@ -69,7 +69,7 @@ extern "C" {
  */
 
 /* Release version as a decimal */
-#define RAPTOR_VERSION_DECIMAL 10404
+#define RAPTOR_VERSION_DECIMAL 10405
 
 /* Major version number */
 #define RAPTOR_VERSION_MAJOR 1
@@ -78,10 +78,10 @@ extern "C" {
 #define RAPTOR_VERSION_MINOR 4
 
 /* Release version number */
-#define RAPTOR_VERSION_RELEASE 4
+#define RAPTOR_VERSION_RELEASE 5
 
 /* Version number of package */
-#define VERSION "1.4.4"
+#define VERSION "1.4.5"
 
 
 #ifdef RAPTOR_XML_LIBXML
