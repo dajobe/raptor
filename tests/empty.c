434d43884e0ed7901f5b31a51032f436a9cf1a3c
Dave Beckett <dave@dajobe.org>
2006-01-08 08:01:27 +0000

    RCS ID and Copyright 2006

--- tests/empty.c
+++ tests/empty.c
@@ -2,11 +2,8 @@
  *
  * empty.c - Raptor empty file test
  *
- * $Id$
- *
- * Copyright (C) 2004-2004, David Beckett http://purl.org/net/dajobe/
- * Institute for Learning and Research Technology http://www.ilrt.bristol.ac.uk/
- * University of Bristol, UK http://www.bristol.ac.uk/
+ * Copyright (C) 2004-2006, David Beckett http://purl.org/net/dajobe/
+ * Copyright (C) 2004-2004, University of Bristol, UK http://www.bristol.ac.uk/
  * 
  * This package is Free Software and part of Redland http://librdf.org/
  * 
