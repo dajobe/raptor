a2d6458e2b66726b0539b63c8e2e16f554a32f16
Dave Beckett <dave@dajobe.org>
2006-01-08 07:52:42 +0000

    1.4.9

--- src/win32_raptor_config.h
+++ src/win32_raptor_config.h
@@ -73,7 +73,7 @@ extern "C" {
  */
 
 /* Release version as a decimal */
-#define RAPTOR_VERSION_DECIMAL 10408
+#define RAPTOR_VERSION_DECIMAL 10409
 
 /* Major version number */
 #define RAPTOR_VERSION_MAJOR 1
@@ -82,10 +82,10 @@ extern "C" {
 #define RAPTOR_VERSION_MINOR 4
 
 /* Release version number */
-#define RAPTOR_VERSION_RELEASE 8
+#define RAPTOR_VERSION_RELEASE 9
 
 /* Version number of package */
-#define VERSION "1.4.8"
+#define VERSION "1.4.9"
 
 
 #ifdef RAPTOR_XML_LIBXML
