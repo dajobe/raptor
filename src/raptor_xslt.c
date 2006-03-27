ca8149321a53f8f38768d21fc28e032fd2406284
Dave Beckett <dave@dajobe.org>
2006-03-27 06:19:53 +0000

    (raptor_init_parser_grddl): Register HTML and XHTML mime types at low q.

--- src/raptor_xslt.c
+++ src/raptor_xslt.c
@@ -465,8 +465,11 @@ raptor_xslt_parser_register_factory(raptor_parser_factory *factory)
 void
 raptor_init_parser_grddl(void)
 {
-  raptor_parser_register_factory("grddl",  "GRDDL over XHTML/XML using XSLT",
-                                 NULL,
-                                 NULL,
-                                 &raptor_xslt_parser_register_factory);
+  raptor_parser_factory* factory;
+  factory=raptor_parser_register_factory("grddl",  "GRDDL over XHTML/XML using XSLT",
+                                         NULL,
+                                         NULL,
+                                         &raptor_xslt_parser_register_factory);
+  raptor_parser_factory_add_mime_type(factory, "text/html", 2);
+  raptor_parser_factory_add_mime_type(factory, "application/html+xml", 2);
 }
