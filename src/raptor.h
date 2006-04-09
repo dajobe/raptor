d4a035e067797c8ee228151d8250b587d2149297
Dave Beckett <dave@dajobe.org>
2006-04-09 21:30:06 +0000

    raptor_xml_element_declare_namespace now has an int return value

--- src/raptor.h
+++ src/raptor.h
@@ -1288,7 +1288,7 @@ raptor_qname** raptor_xml_element_get_attributes(raptor_xml_element* xml_element
 RAPTOR_API
 int raptor_xml_element_get_attributes_count(raptor_xml_element* xml_element);
 RAPTOR_API
-void raptor_xml_element_declare_namespace(raptor_xml_element* xml_element, raptor_namespace *nspace);
+int raptor_xml_element_declare_namespace(raptor_xml_element* xml_element, raptor_namespace *nspace);
 RAPTOR_API
 int raptor_iostream_write_xml_element(raptor_iostream *iostr, raptor_xml_element *element, raptor_namespace_stack *nstack, int is_empty, int is_end, raptor_simple_message_handler error_handler, void *error_data, int depth);
 RAPTOR_API
