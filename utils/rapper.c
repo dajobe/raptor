fe5c7245dac55c95de647f7dce9677307b93dfd4
Dave Beckett <dave@dajobe.org>
2008-03-30 04:39:37 +0000

    2008

--- utils/rapper.c
+++ utils/rapper.c
@@ -2,7 +2,7 @@
  *
  * rapper.c - Raptor RDF Parsing and Serializing utility
  *
- * Copyright (C) 2000-2007, David Beckett http://purl.org/net/dajobe/
+ * Copyright (C) 2000-2008, David Beckett http://www.dajobe.org/
  * Copyright (C) 2000-2005, University of Bristol, UK http://www.bristol.ac.uk/
  * 
  * This package is Free Software and part of Redland http://librdf.org/
