aff855c246413234914cf1c844b4b321f4265f6b
Dave Beckett <dave@dajobe.org>
2008-08-27 15:15:10 +0000

    Added raptor_memnstr()

--- src/raptor_internal.h
+++ src/raptor_internal.h
@@ -1197,6 +1197,9 @@ int raptor_json_writer_blank_object(raptor_json_writer* json_writer, const char*
 int raptor_json_writer_uri_object(raptor_json_writer* json_writer, raptor_uri* uri);
 int raptor_json_writer_key_uri_value(raptor_json_writer* json_writer, const char* key, size_t key_len, raptor_uri* uri);
 
+/* raptor_memnstr.c */
+const char* raptor_memnstr(const char *haystack, size_t haystack_len, const char *needle);
+
 /* raptor_serialize_rdfxmla.c special functions for embedding rdf/xml */
 int raptor_rdfxmla_serialize_set_write_rdf_RDF(raptor_serializer* serializer, int value);
 int raptor_rdfxmla_serialize_set_xml_writer(raptor_serializer* serializer, raptor_xml_writer* xml_writer, raptor_namespace_stack *nstack);
