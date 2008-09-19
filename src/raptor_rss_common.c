82ff1d98f948fadb2d560e0d32872dd4ff8a1fae
Lauri Aalto <laalto@iki.fi>
2008-09-19 20:53:07 +0000

    (raptor_new_rss_item): Init identifier.world as well

--- src/raptor_rss_common.c
+++ src/raptor_rss_common.c
@@ -346,6 +346,7 @@ raptor_new_rss_item(raptor_world* world)
     return NULL;
   
   item->world=world;
+  item->identifier.world=world;
   item->triples=raptor_new_sequence((raptor_sequence_free_handler*)raptor_free_statement, (raptor_sequence_print_handler*)raptor_print_statement);
   if(!item->triples) {
     RAPTOR_FREE(raptor_rss_item, item);
