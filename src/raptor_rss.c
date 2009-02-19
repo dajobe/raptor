b892c96a400dbf79cc31c4442633718ca1510e46
Dave Beckett <dave@dajobe.org>
2009-02-19 01:34:05 +0000

    (raptor_rss_add_container): Added, pulled out of raptor_rss_start_element_handler to include the logic for determining feed type and container from element name. (raptor_rss_start_element_handler): Call above.

--- src/raptor_rss.c
+++ src/raptor_rss.c
@@ -259,6 +259,47 @@ raptor_rss_parse_start(raptor_parser *rdf_parser)
 
 
 
+static int
+raptor_rss_add_container(raptor_rss_parser *rss_parser, const char *name) 
+{
+  raptor_rss_type type = RAPTOR_RSS_UNKNOWN;
+    
+  if(!strcmp(name, "rss") || !strcmp(name, "rdf") || !strcmp(name, "RDF")) {
+    /* rss */
+  } else if(!raptor_strcasecmp(name, "channel")) {
+    /* rss or atom 0.3 channel */
+    type = RAPTOR_RSS_CHANNEL;
+  } else if(!strcmp(name, "feed")) {
+    /* atom 1.0 feed */
+    type = RAPTOR_RSS_CHANNEL;
+    rss_parser->is_atom = 1;
+  } else if(!strcmp(name, "item")) {
+    type = RAPTOR_RSS_ITEM;
+  } else if(!strcmp(name, "entry")) {
+    type = RAPTOR_RSS_ITEM;
+    rss_parser->is_atom = 1;
+  } else {
+    int i;
+    for(i = 0; i < RAPTOR_RSS_COMMON_SIZE; i++)
+      if(!strcmp(name, raptor_rss_types_info[i].name)) {
+        type = (raptor_rss_type)i;
+        break;
+      }
+  }
+    
+  if(type != RAPTOR_RSS_UNKNOWN) {
+    if(type == RAPTOR_RSS_ITEM)
+      raptor_rss_model_add_item(&rss_parser->model);
+    else
+      raptor_rss_model_add_common(&rss_parser->model, type);
+
+    rss_parser->current_type = type;
+  }
+  
+  return (type == RAPTOR_RSS_UNKNOWN);
+}
+
+
 static void
 raptor_rss_start_element_handler(void *user_data,
                                  raptor_xml_element* xml_element)
@@ -304,45 +345,13 @@ raptor_rss_start_element_handler(void *user_data,
 
   /* No container type - identify and record in rss_parser->current_type) */
   if(rss_parser->current_type == RAPTOR_RSS_NONE) {
-    if(!strcmp((const char*)name, "rss") || 
-       !strcmp((const char*)name, "rdf") || 
-       !strcmp((const char*)name, "RDF")) {
-      /* rss */
-    } else if(!raptor_strcasecmp((const char*)name, "channel")) {
-      /* rss or atom 0.3 channel */
-      rss_parser->current_type = RAPTOR_RSS_CHANNEL;
-    } else if(!strcmp((const char*)name, "feed")) {
-      /* atom 1.0 feed */
-      rss_parser->current_type = RAPTOR_RSS_CHANNEL;
-      rss_parser->is_atom = 1;
-    } else if(!strcmp((const char*)name, "item")) {
-      raptor_rss_model_add_item(&rss_parser->model);
-      rss_parser->current_type = RAPTOR_RSS_ITEM;
-    } else if(!strcmp((const char*)name, "entry")) {
-      raptor_rss_model_add_item(&rss_parser->model);
-      rss_parser->current_type = RAPTOR_RSS_ITEM;
-      rss_parser->is_atom = 1;
-    } else {
-      int i;
-      rss_parser->current_type = RAPTOR_RSS_UNKNOWN;
-      for(i=0; i<RAPTOR_RSS_COMMON_SIZE; i++)
-        if(!strcmp((const char*)name, raptor_rss_types_info[i].name)) {
-          rss_parser->current_type = (raptor_rss_type)i;
-          break;
-        }
-    }
-    
-    if(rss_parser->current_type == RAPTOR_RSS_UNKNOWN) {
-      RAPTOR_DEBUG2("Unknown start element named %s\n", name);
+    if(raptor_rss_add_container(rss_parser, (const char*)name)) {
+      RAPTOR_DEBUG2("Unknown container element named %s\n", name);
       /* Nothing more that can be done with unknown element - skip it */
       return;
-    } else {
-      RAPTOR_DEBUG3("FOUND type %d - %s\n", rss_parser->current_type,
+    } else
+      RAPTOR_DEBUG3("FOUND container type %d - %s\n", rss_parser->current_type,
                     raptor_rss_types_info[rss_parser->current_type].name);
-      if (rss_parser->current_type != RAPTOR_RSS_ITEM)
-        raptor_rss_model_add_common(&rss_parser->model, 
-                                    rss_parser->current_type);
-    }
 
     /* have got type so move on to attributes */
     goto check_attributes;
