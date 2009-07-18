f98fd9ae31c49dd39372dbb9e7deaa120e6b3eed
Dave Beckett <dave@dajobe.org>
2009-07-18 04:10:17 +0000

    (raptor_rss_uplift_fields): Comment out variables used only in debugging.

--- src/raptor_rss.c
+++ src/raptor_rss.c
@@ -1414,8 +1414,10 @@ raptor_rss_uplift_fields(raptor_rss_parser* rss_parser, raptor_rss_item* item)
   
   /* COPY some fields from atom to rss/dc */
   for(i = 0; raptor_atom_to_rss[i].from != RAPTOR_RSS_FIELD_UNKNOWN; i++) {
+#ifdef RAPTOR_DEBUG
     raptor_rss_fields_type from_field = raptor_atom_to_rss[i].from;
     raptor_rss_fields_type to_field = raptor_atom_to_rss[i].to;
+#endif
 
     if(raptor_rss_copy_field(rss_parser, item, &raptor_atom_to_rss[i]))
       continue;
