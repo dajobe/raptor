3f44e7887ac18356650b5ab82d0efa132ad82cfe
Dave Beckett <dave@dajobe.org>
2010-12-01 23:11:45 -0800

    Bumped version to 1.9.2

--- src/win32_raptor_config.h
+++ src/win32_raptor_config.h
@@ -90,7 +90,7 @@ extern "C" {
  */
 
 /* Release version as a decimal */
-#define RAPTOR_VERSION_DECIMAL 10901
+#define RAPTOR_VERSION_DECIMAL 10902
 
 /* Major version number */
 #define RAPTOR_VERSION_MAJOR 1
@@ -99,10 +99,10 @@ extern "C" {
 #define RAPTOR_VERSION_MINOR 9
 
 /* Release version number */
-#define RAPTOR_VERSION_RELEASE 1
+#define RAPTOR_VERSION_RELEASE 2
 
 /* Version number of package */
-#define VERSION "1.9.1"
+#define VERSION "1.9.2"
 
 
 #ifdef RAPTOR_XML_LIBXML
