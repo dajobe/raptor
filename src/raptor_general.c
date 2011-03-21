bc4dc28ae6dab2e922b0b44a19a665fdbed02ddb
Dave Beckett <dave@dajobe.org>
2011-03-20 17:25:13 -0700

    Error out when calling library presents a too old header
    (raptor_new_world_internal): Switch to checking for too old, not
    exact match.

--- src/raptor_general.c
+++ src/raptor_general.c
@@ -2,7 +2,7 @@
  *
  * raptor_general.c - Raptor general routines
  *
- * Copyright (C) 2000-2010, David Beckett http://www.dajobe.org/
+ * Copyright (C) 2000-2011, David Beckett http://www.dajobe.org/
  * Copyright (C) 2000-2005, University of Bristol, UK http://www.bristol.ac.uk/
  * 
  * This package is Free Software and part of Redland http://librdf.org/
@@ -50,9 +50,9 @@
 
 /* statics */
 
-const char * const raptor_short_copyright_string = "Copyright 2000-2010 David Beckett. Copyright 2000-2005 University of Bristol";
+const char * const raptor_short_copyright_string = "Copyright 2000-2011 David Beckett. Copyright 2000-2005 University of Bristol";
 
-const char * const raptor_copyright_string = "Copyright (C) 2000-2010 David Beckett - http://www.dajobe.org/\nCopyright (C) 2000-2005 University of Bristol - http://www.bristol.ac.uk/";
+const char * const raptor_copyright_string = "Copyright (C) 2000-2011 David Beckett - http://www.dajobe.org/\nCopyright (C) 2000-2005 University of Bristol - http://www.bristol.ac.uk/";
 
 const char * const raptor_license_string = "LGPL 2.1 or newer, GPL 2 or newer, Apache 2.0 or newer.\nSee http://librdf.org/raptor/LICENSE.html for full terms.";
 
@@ -122,10 +122,10 @@ raptor_new_world_internal(unsigned int version_decimal)
 {
   raptor_world *world;
   
-  if(version_decimal != RAPTOR_VERSION) {
+  if(version_decimal < RAPTOR_MIN_VERSION_DECIMAL) {
     fprintf(stderr,
-            "Raptor compiled with version %u but raptor_new_world() was initialised with version %u\n",
-            RAPTOR_VERSION, version_decimal);
+            "raptor_new_world() called via header from version %u but minimum supported version is %u\n",
+            version_decimal, RAPTOR_MIN_VERSION_DECIMAL);
     return NULL;
   }
   
