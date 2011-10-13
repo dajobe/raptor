b150b409cde420d0af543dd19c0047ae143f995c
Jiří Procházka <ojirio@gmail.com>
2011-10-13 03:34:11 +0200

    added 2 functions for writing uris instead of terms/nodes which is what I really need

--- src/raptor_serialize_turtle.c
+++ src/raptor_serialize_turtle.c
@@ -1193,6 +1193,136 @@ raptor_init_serializer_turtle(raptor_world* world)
 
 
 /**
+ * raptor_uri_write_turtle:
+ * @t1: iostream for writing
+ * @t2: uri
+ * @t3: namespace stack
+ * @t4: base URI
+ *
+ * Write #raptor_uri to a stream in turtle syntax (using QNames).
+ *
+ * Return value: non-0 on failure
+ */
+int
+raptor_uri_write_turtle(raptor_world *world,
+                        raptor_iostream* iostr,
+                        raptor_uri* uri, 
+                        raptor_namespace_stack *nstack,
+                        raptor_uri *base_uri)
+{
+  raptor_qname* qname;
+  raptor_turtle_writer* turtle_writer;
+  
+  turtle_writer = raptor_new_turtle_writer(world, base_uri, 0, nstack, iostr);
+  if(!turtle_writer) {
+    return 1;
+  }
+  
+  qname = raptor_new_qname_from_namespace_uri(nstack, uri, 10);
+
+  /* XML Names allow leading '_' and '.' anywhere but Turtle does not */
+  if(qname && !raptor_turtle_is_legal_turtle_qname(qname)) {
+    raptor_free_qname(qname);
+    qname = NULL;
+  }
+
+  if(qname) {
+    raptor_turtle_writer_qname(turtle_writer, qname);
+    
+    raptor_free_qname(qname);
+  } else {
+    raptor_turtle_writer_reference(turtle_writer, uri);
+  }
+  
+  raptor_free_turtle_writer(turtle_writer);
+  
+  return 0;
+}
+
+
+
+/**
+ * raptor_term_write_turtle:
+ * @t1: iostream for writing
+ * @t2: term
+ * @t3: namespace stack
+ * @t4: base URI
+ *
+ * Write #raptor_term to a stream in turtle syntax (using QNames).
+ *
+ * Return value: non-0 on failure
+ */
+int
+raptor_term_write_turtle(raptor_iostream* iostr,
+                         raptor_term* term, 
+                         raptor_namespace_stack *nstack,
+                         raptor_uri *base_uri)
+{
+  int rc = 0;
+  raptor_turtle_writer* turtle_writer;
+  
+  turtle_writer = raptor_new_turtle_writer(term->world, base_uri, 0, nstack, iostr);
+  if(!turtle_writer) {
+    return 1;
+  }
+  
+  if(term->type == RAPTOR_TERM_TYPE_URI) {
+    rc = raptor_uri_write_turtle(term->world, iostr, term->value.uri, nstack, base_uri);
+  } else if(term->type == RAPTOR_TERM_TYPE_LITERAL) {
+    raptor_turtle_writer_literal(turtle_writer, nstack,
+                                 term->value.literal.string,
+                                 term->value.literal.language, 
+                                 term->value.literal.datatype);
+  } else {
+    rc = 2;
+  }
+  
+  raptor_free_turtle_writer(turtle_writer);
+  
+  return rc;
+}
+
+
+
+/**
+ * raptor_uri_to_turtle_string:
+ * @t1: uri
+ * @t2: namespace stack
+ * @t3: base URI
+ *
+ * Convert #raptor_uri to a string.
+ * Caller has responsibility to free the string.
+ *
+ * Return value: NULL on failure
+ */
+unsigned char*
+raptor_uri_to_turtle_string(raptor_world *world,
+                            raptor_uri* uri, 
+                            raptor_namespace_stack *nstack,
+                            raptor_uri *base_uri)
+{
+  int rc;
+  raptor_iostream* iostr;
+  unsigned char *s;
+  iostr = raptor_new_iostream_to_string(world,
+                                        (void**)&s, NULL, malloc);
+  if(!iostr)
+    return NULL;
+
+  rc = raptor_uri_write_turtle(world, iostr, uri, nstack, base_uri);
+
+  raptor_free_iostream(iostr);
+  if(rc) {
+    free(s);
+    s = NULL;
+  }
+  
+  return s;
+}
+
+
+
+/**
  * raptor_term_to_turtle_string:
  * @t1: term
  * @t2: namespace stack
@@ -1225,64 +1355,4 @@ raptor_term_to_turtle_string(raptor_term* term,
   }
   
   return s;
-}
-
-
-/**
- * raptor_term_write_turtle:
- * @t1: iostream for writing
- * @t2: term
- * @t3: namespace stack
- * @t4: base URI
- *
- * Convert #raptor_term to a string.
- * Caller has responsibility to free the string.
- *
- * Return value: non-0 on failure
- */
-int
-raptor_term_write_turtle(raptor_iostream* iostr,
-                         raptor_term* term, 
-                         raptor_namespace_stack *nstack,
-                         raptor_uri *base_uri)
-{
-  int rc = 0;
-  raptor_qname* qname;
-  raptor_turtle_writer* turtle_writer;
-  
-  turtle_writer = raptor_new_turtle_writer(term->world, base_uri, 0, nstack, iostr);
-  if(!turtle_writer) {
-    return 1;
-  }
-  
-  qname = NULL;
-  
-  if(term->type == RAPTOR_TERM_TYPE_URI) {
-    qname = raptor_new_qname_from_namespace_uri(nstack, term->value.uri, 10);
-    
-    /* XML Names allow leading '_' and '.' anywhere but Turtle does not */
-    if(qname && !raptor_turtle_is_legal_turtle_qname(qname)) {
-      raptor_free_qname(qname);
-      qname = NULL;
-    }
-    
-    if(qname) {
-      raptor_turtle_writer_qname(turtle_writer, qname);
-      
-      raptor_free_qname(qname);
-    } else {
-      raptor_turtle_writer_reference(turtle_writer, term->value.uri);
-    }
-  } else if(term->type == RAPTOR_TERM_TYPE_LITERAL) {
-    raptor_turtle_writer_literal(turtle_writer, nstack,
-                                 term->value.literal.string,
-                                 term->value.literal.language, 
-                                 term->value.literal.datatype);
-  } else {
-    rc = 2;
-  }
-  
-  raptor_free_turtle_writer(turtle_writer);
-  
-  return rc;
 }
