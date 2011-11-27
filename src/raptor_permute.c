8afeb53b4704bd7f1efd37f5a2d2ef11b2f99434
Dave Beckett <dave@dajobe.org>
2011-11-27 09:30:38 -0800

    Turn permute code into a test for sequence raptor_sequence_next_permutation

--- src/raptor_permute.c
+++ src/raptor_permute.c
@@ -1,6 +1,6 @@
 /* -*- Mode: c; c-basic-offset: 2 -*-
  *
- * permute.c - Permutations of elements in a sequence
+ * raptor_permute.c - Test of permutations of ints in a sequence
  *
  * Copyright (C) 2011, David Beckett http://www.dajobe.org/
  * 
@@ -38,29 +38,39 @@
 #include "raptor_internal.h"
 
 
-#define REVERSE_DEBUG 0
 #define PERMUTE_DEBUG 0
 
 
 typedef struct 
 {
+  raptor_sequence* seq;
   int* contents;
-  size_t size;
 } intseq;
 
 
+static int
+print_handler(void *object, FILE *fh)
+{
+  int* pi = (int*)object;
+  fprintf(fh, "%d", *pi);
+  return 0;
+}
+
+
 static intseq*
-new_intseq(size_t size) 
+new_intseq(int size) 
 {
   intseq* iseq;
   int i;
   
   iseq = RAPTOR_CALLOC(intseq*, 1, sizeof(*iseq));
-  iseq->size = size;
   iseq->contents = RAPTOR_CALLOC(int*, size, sizeof(int));
+  /* will be a sequence of int* pointing into iseq->contents */
+  iseq->seq = raptor_new_sequence(NULL, print_handler);
 
   for(i = 0; i < (int)size; i++) {
     iseq->contents[i] = i + 1;
+    raptor_sequence_set_at(iseq->seq, i, &iseq->contents[i]);
   }
 
   return iseq;
@@ -72,137 +82,38 @@ free_intseq(intseq* iseq)
   if(iseq->contents)
     RAPTOR_FREE(int*, iseq->contents);
 
+  if(iseq->seq)
+    raptor_free_sequence(iseq->seq);
+
   RAPTOR_FREE(intseq*, iseq);
 }
 
-
+#if PERMUTE_DEBUG > 0
 static void
 intseq_print(intseq *iseq, FILE* stream)
 {
-  size_t i;
-
-  for(i = 0; i < iseq->size; i++) {
-    if(i > 0)
-      fputc(' ', stream);
-    fprintf(stream, "%d", iseq->contents[i]);
-  }
-  
+  raptor_sequence_print(iseq->seq, stream);
 }
-
-
-/**
- * intseq_reverse:
- * @iseq: int seq
- * @start_index: starting index
- * @length: number of elements to reverse
- *
- * Reverse a range of elements
- *
- * Return value: non-0 if arguments are out of range
- */
-static int
-intseq_reverse(intseq *iseq, size_t start_index, size_t length) 
-{
-  size_t end_index = start_index + length - 1;
-
-#if REVERSE_DEBUG > 0
-  fprintf(stderr, "intseq_reverse(%d, %d) end_index %d array size %d\n",
-          (int)start_index, (int)length, (int)end_index, (int)iseq->size);
-
-  fputs("  initial: ", stderr);
-  intseq_print(iseq, stderr);
-  fputc('\n', stderr);
-#endif
-  if(end_index >= iseq->size)
-    return 1;
-  
-  if(length == 1)
-    goto done;
-
-  while( (start_index != end_index) && (start_index != end_index + 1) ) {
-    int tmp;
-    tmp = iseq->contents[start_index];
-    iseq->contents[start_index] = iseq->contents[end_index];
-    iseq->contents[end_index] = tmp;
-    start_index++; end_index--;
-  }
-
-  done:
-#if REVERSE_DEBUG > 0
-  fputs("    final: ", stderr);
-  intseq_print(iseq, stderr);
-  fputc('\n', stderr);
 #endif
 
-  return 0;
+static int
+intseq_reverse(intseq *iseq, int start_index, int length) 
+{
+  return raptor_sequence_reverse(iseq->seq, start_index, length);
 }
 
-
 static int
-intseq_compare_at(intseq* iseq, size_t index1, size_t index2) 
+intseq_compare_at(const void* a, const void* b)
 {
-  return iseq->contents[index1] - iseq->contents[index2];
+  int* ia = (int*)a;
+  int* ib = (int*)b;
+  return *ia - *ib;
 }
 
-
-/**
- * intseq_next_permutation:
- * @iseq: int seq
- *
- * Permute in lexicographic order
- *
- * Assumes the initial order of the items is lexicographically
- * increasing.  This function alters the order of the items until the
- * last permuatation is done at which point the contents is reset to
- * the intial order.
- *
- * Algorithm used is described in
- * <http://en.wikipedia.org/wiki/Permutation#Generation_in_lexicographic_order>
- *
- * Return value: non-0 at the last  permutation
- */
-
 static int
-intseq_next_permutation(intseq *iseq) 
+intseq_next_permutation(intseq *iseq)
 {
-  long k;
-  long l;
-  int temp;
-  
-  if(iseq->size < 2)
-    return 1;
-  
-  /* 1. Find the largest index k such that a[k] < a[k + 1]. If no such
-   * index exists, the permutation is the last permutation.
-   */
-  k = iseq->size - 2;
-  while(k >= 0 && intseq_compare_at(iseq, k, k + 1) >= 0)
-    k--;
-  
-  if(k == -1) {
-    /* done - reset to starting order */
-    intseq_reverse(iseq, 0, iseq->size);
-    return 1;
-  }
-  
-  /* 2. Find the largest index l such that a[k] < a[l]. Since k + 1
-   * is such an index, l is well defined and satisfies k < l.
-   */
-  l = iseq->size - 1;
-  while( intseq_compare_at(iseq, k, l) >= 0)
-    l--;
-  
-  /* 3. Swap a[k] with a[l]. */
-  temp = iseq->contents[k];
-  iseq->contents[k] = iseq->contents[l];
-  iseq->contents[l] = temp;
-
-  /* 4. Reverse the sequence from a[k + 1] up to and including the
-   * final element a[n].
-   */
-  intseq_reverse(iseq, k + 1, iseq->size - (k + 1));
-
-  return 0;
+  return raptor_sequence_next_permutation(iseq->seq, intseq_compare_at);
 }
 
 
@@ -226,12 +137,14 @@ int main (int argc, char *argv[])
 
     iseq = new_intseq(size);
 
+#if PERMUTE_DEBUG > 0
     fprintf(stderr, "%s: Permutation test %d.  Initial state: ", program, size);
     intseq_print(iseq, stderr);
     fputc('\n', stderr);
+#endif
 
     for(count = 1; 1; count++) {
-#if PERMUTE_DEBUG > 0
+#if PERMUTE_DEBUG > 1
       fprintf(stderr, "Permutation %3d: ", count);
       intseq_print(iseq, stderr);
       fputc('\n', stderr);
@@ -240,9 +153,11 @@ int main (int argc, char *argv[])
         break;
     }
 
+#if PERMUTE_DEBUG > 0
     fprintf(stderr, "%s: Returned %d results.  Final state: ", program, count);
     intseq_print(iseq, stderr);
     fputc('\n', stderr);
+#endif
 
     free_intseq(iseq);
 
