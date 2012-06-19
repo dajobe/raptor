bfcc1d158dedae865a9410796e44c1390d197a5f
Dave Beckett <dave@dajobe.org>
2012-06-19 13:43:28 -0700

    Update rdfguess example code to read from stdin or a file called '-'
    (rdfguess_guess_name): Added and alter to accept file called '-' as
    reading from FILE* stdin.
    
    (main): Call above to support reading from stdin.
    Do something with -h/--help

--- examples/rdfguess.c
+++ examples/rdfguess.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #include <raptor2.h>
 
@@ -9,65 +10,90 @@
 
 #define READ_BUFFER_SIZE 256
 
+static int
+rdfguess_guess_name(raptor_world* world, const char* filename)
+{
+  char *buffer[READ_BUFFER_SIZE];
+  raptor_iostream* iostr = NULL;
+  const char* name;
+  const unsigned char* identifier;
+  const char* label;
+  size_t read_len;
+  size_t count;
+  
+  if(!strcmp(filename, "-")) {
+    iostr = raptor_new_iostream_from_file_handle(world, stdin);
+    identifier = NULL;
+    label = "<stdin>";
+  } else {
+    if(access(filename, R_OK)) {
+      fprintf(stderr, "rdfguess: file %s not found\n", filename);
+      return 1;
+    }
+    
+    iostr = raptor_new_iostream_from_filename(world, filename);
+    identifier = (const unsigned char *)filename;
+    label = filename;
+  }
+  
+  if(!iostr) {
+    fprintf(stderr, "rdfguess: Could not create iostream for %s\n", label);
+    goto tidy;
+  }
+
+  read_len = READ_BUFFER_SIZE;
+  count = raptor_iostream_read_bytes(buffer, 1, read_len, iostr);
+  if(count < 1) {
+    fprintf(stderr, "rdfguess: Failed to read any data from %s\n",
+            label);
+    goto tidy;
+  }
+  
+  name = raptor_world_guess_parser_name(world,
+                                        /* uri*/ NULL,
+                                        /* mime_type */ NULL,
+                                        (const unsigned char*)buffer,
+                                        read_len, 
+                                        identifier);
+                                        
+  if(name)
+    fprintf(stdout, "rdfguess: %s guessed to be %s\n", label, name);
+  else
+    fprintf(stdout, "rdfguess: failed to guess parser for %s\n", label);
+  
+  tidy:
+  raptor_free_iostream(iostr);
+    
+  return 0;
+}
+      
+
 int
 main(int argc, char *argv[])
 {
   raptor_world *world = NULL;
-  char *buffer[READ_BUFFER_SIZE];
   const char *filename;
-  int rc = 1;
-  int i;
+  int rc = 0;
   
   world = raptor_new_world();
 
-  if(argc < 2) {
-    fprintf(stderr, "USAGE rdfguess: FILENAMES...\n");
+  if(argc == 2 && (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help"))) {
+    fprintf(stderr, "USAGE rdfguess [FILENAMES...]\n");
+    rc = 1;
     goto tidy;
   }
-  
-  for(i = 1; (filename = (const char*)argv[i]); i++) {
-    raptor_iostream* iostr = NULL;
-    const char* name;
-    size_t read_len;
-    size_t count;
 
-    if(access(filename, R_OK)) {
-      fprintf(stderr, "rdfguess: %s not found\n", filename);
-      goto tidy;
+  if(argc == 1) {
+    rc = rdfguess_guess_name(world, "-");
+  } else {
+    int i;
+
+    for(i = 1; (filename = (const char*)argv[i]); i++) {
+      rc = rdfguess_guess_name(world, filename);
+      if(rc)
+        break;
     }
-
-    iostr = raptor_new_iostream_from_filename(world, filename);
-    if(!iostr) {
-      fprintf(stderr, "rdfguess: Could not create iostream for %s\n", filename);
-      goto tidy;
-    }
-
-    read_len = READ_BUFFER_SIZE;
-    count = raptor_iostream_read_bytes(buffer, 1, read_len, iostr);
-    if(count < 1) {
-      fprintf(stderr, "rdfguess: Failed to read any data from file %s\n",
-              filename);
-      raptor_free_iostream(iostr);
-      goto tidy;
-    }
-
-
-    name = raptor_world_guess_parser_name(world,
-                                          /* uri*/ NULL,
-                                          /* mime_type */ NULL,
-                                          (const unsigned char*)buffer,
-                                          read_len, 
-                                          /* identifier */ (const unsigned char *)filename);
-
-    if(name)
-      fprintf(stdout, "rdfguess: %s guessed to be %s\n", filename, name);
-    else
-      fprintf(stdout, "rdfguess: failed to guess parser for %s\n", filename);
-
-    raptor_free_iostream(iostr);
   }
-  
-  rc = 0;
 
   tidy:
   raptor_free_world(world);
