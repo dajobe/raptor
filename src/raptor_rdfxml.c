b8156a90d377e912e58a2b53cf6c8f253710907f
Dave Beckett <dave@dajobe.org>
2012-08-14 14:57:43 -0700

    (raptor_rdfxml_element_content_type_as_string): LAST is not valid

--- src/raptor_rdfxml.c
+++ src/raptor_rdfxml.c
@@ -301,8 +301,9 @@ static const struct {
 static const char *
 raptor_rdfxml_element_content_type_as_string(raptor_rdfxml_element_content_type type) 
 {
-  if(type > RAPTOR_RDFXML_ELEMENT_CONTENT_TYPE_LAST)
+  if(type >= RAPTOR_RDFXML_ELEMENT_CONTENT_TYPE_LAST)
     return "INVALID";
+
   return rdf_content_type_info[type].name;
 }
 
