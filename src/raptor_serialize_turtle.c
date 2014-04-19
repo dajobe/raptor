e3d0549c1eaa27e11d84481d53c6753455190c93
Dave Beckett <dave@dajobe.org>
2014-04-18 17:11:35 -0700

    (raptor_turtle_emit_subject): check raptor_avltree_iterator_next() return [coverity CID 29234]

--- src/raptor_serialize_turtle.c
+++ src/raptor_serialize_turtle.c
@@ -622,7 +622,8 @@ raptor_turtle_emit_subject(raptor_serializer *serializer,
     if(!iter)
       return 1;
     pred1 = ((raptor_abbrev_node**)raptor_avltree_iterator_get(iter))[0];
-    raptor_avltree_iterator_next(iter);
+    if(raptor_avltree_iterator_next(iter))
+      return 1;
     pred2 = ((raptor_abbrev_node**)raptor_avltree_iterator_get(iter))[0];
     raptor_free_avltree_iterator(iter);
 
