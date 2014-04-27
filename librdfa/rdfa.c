dajobe/raptor

commit 9447e886ad66a1c3dd2c877341feca639824488a

(rdfa_init_base): Fix uri_start dead code again [coverity CID 29700]

librdfa/rdfa.c
@@ -142,17 +142,20 @@ static size_t rdfa_init_base(
    if(head_end != NULL)
    {
       char* base_start = strstr(*working_buffer, "<base ");
-      if(base_start == NULL)
+      char* href_start = NULL;
+      if(base_start == NULL) {
          base_start = strstr(*working_buffer, "<BASE ");
-
-      if(base_start != NULL)
+         if(base_start != NULL)
+           href_start = strstr(base_start, "href=");
+      }
+      
+      if(href_start != NULL)
       {
-         char* href_start = strstr(base_start, "href=");
          char sep = href_start[5];
          char* uri_start = href_start + 6;
-         char* uri_end = uri_start ? strchr(uri_start, sep) : NULL;
+         char* uri_end = strchr(uri_start, sep);
 
-         if((uri_start != NULL) && (uri_end != NULL))
+         if(uri_end != NULL)
          {
             if(*uri_start != sep)
             {

commit 4b29d69153af83a6f757e4b62bb27faab0a76205

(rdfa_init_base): Fix <base href >
Fixes support broke in commit 9447e886ad66a1c3dd2c877341feca639824488a

librdfa/rdfa.c
@@ -143,11 +143,10 @@ static size_t rdfa_init_base(
    {
       char* base_start = strstr(*working_buffer, "<base ");
       char* href_start = NULL;
-      if(base_start == NULL) {
+      if(base_start == NULL)
          base_start = strstr(*working_buffer, "<BASE ");
-         if(base_start != NULL)
-           href_start = strstr(base_start, "href=");
-      }
+      if(base_start != NULL)
+        href_start = strstr(base_start, "href=");
       
       if(href_start != NULL)
       {