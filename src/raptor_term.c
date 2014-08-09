f0df0625572ee5f73c84c3ef4e194df2f90f854b
Dave Beckett <dave@dajobe.org>
2014-08-09 15:13:01 -0700

    (raptor_new_term_from_blank): Accept "" as same as NULL blank

--- src/raptor_term.c
+++ src/raptor_term.c
@@ -344,8 +344,8 @@ raptor_new_term_from_counted_blank(raptor_world* world,
  *
  * Takes a copy of the passed in @blank
  *
- * If @blank is NULL, creates a new internal identifier and uses it.
- * This will use the handler set with
+ * If @blank is NULL or an empty string, creates a new internal
+ * identifier and uses it.  This will use the handler set with
  * raptor_world_set_generate_bnodeid_parameters()
  *
  * Return value: new term or NULL on failure
@@ -359,8 +359,12 @@ raptor_new_term_from_blank(raptor_world* world, const unsigned char* blank)
 
   raptor_world_open(world);
 
-  if (blank)
-    length = strlen((const char*)blank);
+  if(blank) {
+    if(*blank)
+      length = strlen((const char*)blank);
+    else
+      blank = NULL;
+  }
 
   return raptor_new_term_from_counted_blank(world, blank, length);
 }
