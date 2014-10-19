147575a8a595d472425b628e44c423383665f253
Dave Beckett <dave@dajobe.org>
2014-10-19 10:46:23 -0700

    delete working dir

--- scripts/install-bison3.sh
+++ scripts/install-bison3.sh
@@ -5,6 +5,9 @@ set -x
 
 V=3.0.2
 
+cd /tmp
 wget http://ftp.gnu.org/gnu/bison/bison-$V.tar.gz
 tar -x -z -f bison-$V.tar.gz
 cd bison-$V && ./configure --prefix=/usr && make && sudo make install
+cd ..
+rm -rf bison-$V
