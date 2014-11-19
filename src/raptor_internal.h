c5bdc6984050ba5ff2cbe2a0e30b3becdefe3def
Richard H. McCullough <rhmccullough@gmail.com>
2014-11-18 22:53:17 -0800

    change "." to ";" in @base statement
    raptor_internal.h: add world->emit_mkr for use by
    raptor_turtle_writer_base().
    raptor_new_serializer(): set world->emit_mkr
    tests/mkr/*.mkr: change "." to ";" in expected output files

--- src/raptor_internal.h
+++ src/raptor_internal.h
@@ -1399,6 +1399,7 @@ struct raptor_world_s {
 
   /* sequence of serializer factories */
   raptor_sequence *serializers;
+  int emit_mkr;  /* Non 0 for mKR serializer */
 
   /* raptor_rss_common initialisation counter */
   int rss_common_initialised;
