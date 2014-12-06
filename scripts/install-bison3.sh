f581a24b81861e2bcf67b8c068c6493b76667a60
Dave Beckett <dave@dajobe.org>
2014-12-05 20:40:13 -0800

    Update install bison script to do nothing if bison is new enough

--- scripts/install-bison3.sh
+++ scripts/install-bison3.sh
@@ -1,17 +1,36 @@
 #!/bin/sh
-# Helper script to install newer bison 3 for travis CI
+# Helper script to install bison 3 (primarily for travis CI)
 
 set -x
 
-V=3.0.2
+PACKAGE=bison
+MIN_VERSION=3.0.0
+INSTALL_VERSION=3.0.2
 
-# Bison requires CC is a C compiler
+# Bison requires that CC is a C compiler
 CC=cc
 export CC
 
-cd /tmp
-wget http://ftp.gnu.org/gnu/bison/bison-$V.tar.gz
-tar -x -z -f bison-$V.tar.gz
-cd bison-$V && ./configure --prefix=/usr && make && sudo make install
-cd ..
-rm -rf bison-$V
+AWK=${AWK:-awk}
+BISON=${BISON:-bison}
+CURL=${CURL:-curl}
+WGET=${WGET:-wget}
+
+FILE="$PACKAGE-$INSTALL_VERSION.tar.gz"
+URL="http://ftp.gnu.org/gnu/bison/$PACKAGE-$INSTALL_VERSION.tar.gz"
+
+ROOT_DIR=${TMPDIR:-/tmp}
+BUILD_DIR="$ROOT_DIR/build-$PACKAGE"
+
+installed_version=`$BISON --version 2>&1 | sed -ne 's/^.*GNU Bison[^0-9]*//p'`
+installed_version_dec=`echo $installed_version | $AWK -F. '{printf("%d\n", 10000*$1 + 100*$2 + $3)};'`
+min_version_dec=`echo $MIN_VERSION | $AWK -F. '{printf("%d\n", 10000*$1 + 100*$2 + $3)};'`
+if test $installed_version_dec -ge $min_version_dec; then
+    echo "$PACKAGE $installed_version is new enough"
+else
+    mkdir $BUILD_DIR && cd $BUILD_DIR
+    $WGET -O $FILE $URL || $CURL -o $FILE $URL
+    tar -x -z -f $FILE && rm $FILE
+    cd $PACKAGE-$INSTALL_VERSION && ./configure --prefix=/usr && make && sudo make install
+    cd / && rm -rf $BUILD_DIR
+fi
