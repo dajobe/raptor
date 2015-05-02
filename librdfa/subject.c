44b8d5f2cae6c4e0e4c5c5207dbbd0098ff3df10
Dave Beckett <dave@dajobe.org>
2015-05-02 14:18:53 -0700

    Remove sprintf() and replace with snprintf() or hard-coded format

--- librdfa/subject.c
+++ librdfa/subject.c
@@ -43,7 +43,7 @@ char* rdfa_create_bnode(rdfacontext* context)
    char buffer[64];
    
    /* print and increment the bnode count */
-   sprintf(buffer, "_:bnode%i", (int)context->bnode_count++);
+   snprintf(buffer, 64, "_:bnode%i", (int)context->bnode_count++);
    rval = rdfa_replace_string(rval, buffer);
 
    return rval;
